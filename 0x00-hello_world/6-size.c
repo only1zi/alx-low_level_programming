5530084a46893673573d7472355a7c2ecd9885aa
only1zi <only1zizii@gmail.com>
2023-09-07 16:59:41 -0700

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - function as main
+ *
+ * Return: 0 when successful
+ */
+
+int main(void)
+{
+	printf("size of char: %lu byte(s)", sizeof(char));
+	printf("size of int: %lu byte(s)", sizeof(int));
+	printf("size of a long int: %lu byte(s)", sizeof(long int));
+	printf("size of a long long int: %lu byte(s)", sizeof(long long int));
+	printf("size of float: %lu byte(s)", sizeof(float));
+	return (0);
+}
+
